309a6098528757a5376f154db689e20b40869e34
root <root@ip-10-42-70-120.ec2.internal>
2025-12-18 21:15:51 +0000

    Task 2: Simple Shell 0.1 with prompt, execve, and EOF handling

---
+++ shell.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+ * main - Simple shell 0.1
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Exit status
+ */
+int main(int argc, char **argv)
+{
+char *line = NULL;
+size_t n = 0;
+ssize_t read;
+pid_t pid;
+int status;
+char *args[2];
+
+(void)argc;
+
+while (1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+read = getline(&line, &n, stdin);
+if (read == -1)
+{
+free(line);
+return (0);
+}
+
+if (line[read - 1] == '\n')
+line[read - 1] = '\0';
+
+if (line[0] == '\0')
+continue;
+
+pid = fork();
+if (pid == -1)
+{
+perror(argv[0]);
+continue;
+}
+if (pid == 0)
+{
+args[0] = line;
+args[1] = NULL;
+
+if (execve(args[0], args, environ) == -1)
+{
+perror(argv[0]);
+}
+exit(EXIT_FAILURE);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
+}
+free(line);
+return (0);
+}
