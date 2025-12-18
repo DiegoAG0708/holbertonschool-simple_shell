7311b8d7c2c392780371e0affd2f607275e48b36
root <root@ip-10-42-70-120.ec2.internal>
2025-12-18 21:42:41 +0000

    Final fix: trim both leading and trailing spaces for checker compatibility

--- shell.c
+++ shell.c
@@ -1,6 +1,32 @@
 #include "shell.h"
 
 /**
+ * trim_spaces - Removes leading and trailing spaces from a string
+ * @str: Input string
+ *
+ * Return: Trimmed string (pointer inside original buffer)
+ */
+char *trim_spaces(char *str)
+{
+char *end;
+
+while (*str == ' ')
+str++;
+
+if (*str == '\0')
+return str;
+
+end = str + strlen(str) - 1;
+while (end > str && *end == ' ')
+{
+*end = '\0';
+end--;
+}
+
+return str;
+}
+
+/**
  * main - Simple shell 0.1
  * @argc: Argument count
  * @argv: Argument vector
@@ -12,6 +38,7 @@ int main(int argc, char **argv)
 char *line = NULL;
 size_t n = 0;
 ssize_t read;
+char *cmd;
 pid_t pid;
 int status;
 char *args[2];
@@ -30,27 +57,27 @@ free(line);
 return (0);
 }
 
-/* Trim trailing newline */
-if (line[read - 1] == '\n')
-line[read - 1] = '\0';
+cmd = strtok(line, "\n");
+while (cmd != NULL)
+{
+cmd = trim_spaces(cmd);
 
-/* Trim leading spaces */
-while (*line == ' ')
-line++;
-
-/* Ignore empty or space-only input */
-if (*line == '\0')
+if (*cmd == '\0')
+{
+cmd = strtok(NULL, "\n");
 continue;
+}
 
 pid = fork();
 if (pid == -1)
 {
 perror(argv[0]);
+cmd = strtok(NULL, "\n");
 continue;
 }
 if (pid == 0)
 {
-args[0] = line;
+args[0] = cmd;
 args[1] = NULL;
 
 if (execve(args[0], args, environ) == -1)
@@ -62,6 +89,8 @@ exit(EXIT_FAILURE);
 else
 {
 waitpid(pid, &status, 0);
+cmd = strtok(NULL, "\n");
+}
 }
 }
 free(line);
