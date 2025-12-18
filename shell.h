e725f61ab8ab52b407c577e3c6e14dd0b17f8ba8
root <root@ip-10-42-70-120.ec2.internal>
2025-12-18 21:25:23 +0000

    Task 2: Simple Shell 0.1 with prompt, execve, and EOF handling

--- shell.h
+++ shell.h
@@ -8,29 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
-#include <sys/stat.h>
 
 extern char **environ;
-
-/* main loop */
-char *read_line(void);
-
-/* prompt */
-int is_interactive(void);
-void print_prompt(void);
-
-/* parsing */
-char **tokenize(const char *line);
-void free_tokens(char **argv);
-
-/* builtins */
-int is_builtin(char **argv);
-int run_builtin(char **argv);
-
-/* execution */
-int execute(char **argv, const char *progname, int cmd_idx);
-
-/* path resolution */
-char *find_in_path(const char *cmd);
 
 #endif /* SHELL_H */
